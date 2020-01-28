b52ee2f095b0e70a21a008adbefd54cd093a1a43
Joss <josloiz@yahoo.fr>
2020-01-28 16:35:51 +0900

    Change the game engine to use HaxeFlixel instead of Kha

--- khafile.js
+++
@@ -1,7 +0,0 @@
-let project = new Project('BroughLike');
-project.addAssets('Assets/**');
-project.addSources('Sources');
-project.addShaders('Shaders/**');
-project.addLibrary('raccoon');
-project.addLibrary('delta');
-resolve(project);
